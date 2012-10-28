012fd12299a32ba1fb810ea6ff7c5c0810196601
Will Mullane <will-mull@hotmail.com>
2012-10-28 19:26:38 +0000

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ubetcha::Application.config.secret_token = '7b1458958bb7f026751fe48928b6ba4600a825668280b2ba74e0a8ba1f1464524c7191ab5190366ca7747ff58b8dae99994e6fd54e9e741ebf7974bcdc13c71c'
