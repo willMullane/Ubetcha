dcfff809bf3e24989b252e336d7d2aa91f925b74
Will Mullane <will-mull@hotmail.com>
2013-01-11 22:38:59 +0000

    Scoped status updats to the current user on create and update

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -39,6 +39,17 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to status_path(assigns(:status))
   end
 
+  test "should create status for the current user when logged in" do
+    sign_in users(:will)
+
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content, user_id: users(:brian).id }
+    end
+
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:will).id
+  end
+
   test "should show status" do
     get :show, id: @status
     assert_response :success
@@ -60,6 +71,20 @@ class StatusesControllerTest < ActionController::TestCase
     sign_in users(:will)
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
+  end
+
+  test "should update status for current user when logged in" do
+    sign_in users(:will)
+    put :update, id: @status, status: { content: @status.content, user_id: users(:brian).id }
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:will).id
+  end
+
+  test "should not update status if nothing has changed" do
+    sign_in users(:will)
+    put :update, id: @status
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:will).id
   end
 
   test "should destroy status" do
